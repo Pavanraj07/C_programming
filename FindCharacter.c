59e64811898171f807cce8c35411d267ec5ba012
Pavanraj07 <pavanrajcsd152@gmail.com>
2024-12-30 20:11:42 +0530

    initial commit

---
+++ FindCharacter.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int FindChar(char  str[], char chr, int i )
+{
+
+    if(str[i] == '\0')
+        return -1;
+
+    if( str[i] == chr )
+        return i;
+
+    return FindChar(str, chr, i+1);
+}
+
+int main()
+{
+
+    char str[100];
+    char chr;
+    int i=0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]s",str);
+
+    while( getchar() != '\n' );
+
+    printf("Enter a character ");
+    scanf("%c",&chr);
+
+    int index = FindChar( str, chr, i );
+
+    if( index != -1 )
+        printf("Character found at index %d",index);
+    else
+        printf("String Not found");
+
+    return 0;
+}
