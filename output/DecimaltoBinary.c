4586f7d3f72fb2804c4724ad45449baffa6ff0f1
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-26 11:59:36 +0530

    commit

---
+++ output/DecimaltoBinary.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void decimalToBinary(int n) {
+    if (n == 0)
+        return;
+    decimalToBinary(n / 2);
+    printf("%d", n % 2);
+}
+
+int main() {
+    int num;
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+
+    printf("Binary representation: ");
+    if (num == 0) {
+        printf("0");
+    } else {
+        decimalToBinary(num);
+    }
+    printf("\n");
+
+    return 0;
+}
